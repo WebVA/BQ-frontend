8693330c58b07f054dfef88466c839e95f644739
webva <adrianwang224@gmail.com>
2024-08-30 15:07:57 +0100

    update

--- src/screen/governance/components/stake.tsx
+++ src/screen/governance/components/stake.tsx
@@ -15,7 +15,7 @@ import {
 import { convertAmount, convertTvl } from '@/lib/utils';
 import { GovContract, MockERC20Contract } from '@/constant/contracts';
 import Grid from '~/svg/grid.svg';
-import { addTokenToMetaMask } from '../../../global';
+import { addTokenToMetaMask } from '@/hooks/global';
 
 export const Stake = (): JSX.Element => {
   const [amount, setAmount] = useState<string>('10');
